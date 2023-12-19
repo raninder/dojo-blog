c2768caee54901902cecbaf22ccbdf3d4908cfd9
Raninder Dhillon <raninderdhillon@gmail.com>
2023-12-19 13:40:37 -0500

    update App with router

--- src/App.js
+++ src/App.js
@@ -1,48 +1,18 @@
-
-
 import Navbar from './Navbar';
-// import Home from './Home';
-// import Home1 from './Home1';
-// import Home2 from './Home2';
-// import Home3 from './Home3';
-// import Home4 from './Home4';
-// import Home5 from './Home5';
-// import Home6 from './Home6';
-// import Home7 from './Home7';
 import Home8 from './Home8';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 function App() {
-  //dynamic values in templates
-  // const title= "Welcome to new blog";
-  // const likes = 50;
-  // const link = "https://google.com";
+  
   return (
+    <Router>
     <div className="App">
       <Navbar />
-      {/* <div className="content">
-        <h1>App component</h1>
-        <h2>{ title }</h2>
-        <p>liked { likes } times</p>
-        <p>{ 1 }</p>
-        <p>{ "Hello world" }</p> */}
-        {/* output an array as a string, cant render bopolen and objects */}
-        {/* <p>{ [1,2,3,4,5] }</p> 
-        <p> { Math.random()*10 }</p>   convert to a string  */}
-        {/* <a href= { link } > Google site</a>
-      </div> */}
-
       <div className="content">
-        {/* <Home /> */ }
-        {/* <Home1 />  */}
-        {/* <Home2 /> */}
-        {/* <Home3 /> */}
-        {/* <Home4 /> */}
-        {/* <Home5 /> */}
-        {/* <Home6 /> */}
-        {/* <Home7 /> */}
         <Home8 />
       </div>
     </div>
+    </Router>
   );
 }
 
