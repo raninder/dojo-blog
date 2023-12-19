4f2673557bdf8771652cf015d5d237c71510415e
Raninder Dhillon <raninderdhillon@gmail.com>
2023-12-19 12:49:52 -0500

    Update App_try.js

--- src/App_try.js
+++ src/App_try.js
@@ -1,20 +1,12 @@
+import Navbar from './Navbar';
+import Home from './Home';
 function App() {
-  // dynamic values in templates
-  const title= "Welcome to new blog";
-  const likes = 50;
-  const link = "https://google.com";
+  
   return (
     <div className="App">
+			<Navbar />
       <div className="content">
-        <h1>App component</h1>
-        <h2>{ title }</h2>
-        <p>liked { likes } times</p>
-        <p>{ 1 }</p>
-        <p>{ "Hello world" }</p>
-        {/* output an array as a string, cant render bopolen and objects */}
-        <p>{ [1,2,3,4,5] }</p> 
-        <p> { Math.random()*10 }</p>   convert to a string 
-        <a href= { link } > Google site</a>
+        <Home />
       </div>
 		</div>	
 	)
