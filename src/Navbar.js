f4067e55ce2966530c7d4ecd392814e7fa74540a
Raninder Dhillon <raninderdhillon@gmail.com>
2023-12-20 13:09:05 -0500

    using Link in Navbar instead of a tag

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,20 +1,25 @@
-
+import { Link } from 'react-router-dom'
 // type sfc and press enter key, simple react snippet will generate functional component template
 //sfc is stateless functional component
+
 const  Navbar = () => {
 	return ( 
 		// type nav.navbar and press enter
 		<nav className="navbar">
 		<h1> The Dojo Blog</h1>
 		<div className="links">
-			<a href="/">Home</a>
+			
 			{/* add inline style using {{}} one {}for dynamic variables and other {} as object 
 			style={{ 
           color: 'white', 
           backgroundColor: '#f1356d',
           borderRadius: '8px' 
         }}>*/}
-			<a href="/create">New Blog</a>
+				{/* we are replacing href with Link for react-router-dom to intercept requests in lec 22*/}
+			{/* <a href="/">Home</a>
+			<a href="/create">New Blog</a> */} 
+			<Link to="/">Home</Link>
+			<Link to="/create">New Blog</Link>
 		</div>
 		</nav>
 	 );
