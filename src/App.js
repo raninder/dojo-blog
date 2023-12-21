f989bb8e88e3e2d35c86366a7c48fd7fa5b2f650
Raninder Dhillon <raninderdhillon@gmail.com>
2023-12-21 14:03:01 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Navbar from './Navbar';
-import Home8 from './Home8';
+// import Home8 from './Home8';
 import Create from './Create';
 //using routes instead of Switch in react-rouer-dom ver6
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import BlogDetails1 from './BlogDetails1';
+import Home9 from './Home9';
 
 
 function App() {
@@ -13,8 +15,9 @@ function App() {
         <Navbar />
         <div className="content">
           <Routes>
-          <Route path="/" element={<Home8 />} />
+          <Route path="/" element={<Home9 />} />
           <Route path="/create" element={<Create />} />
+          <Route path="/blogs/:id" element={<BlogDetails1 />} />
             </Routes>
         
         </div>
