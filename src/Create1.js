827dbdb744c31106133c8c8bb3b6827383ebeeef
Raninder Dhillon <raninderdhillon@gmail.com>
2023-12-22 11:35:59 -0500

    controlled form data

--- src/Create1.js
+++ src/Create1.js
@@ -1,7 +1,9 @@
 import { useState } from "react";
 
 const Create1 = () => {
-	const [title,setTitle] = useState();
+	const [title,setTitle] = useState('');
+	const [body,setBody] = useState('');
+	const [author,setAuthor] = useState('mario');
 	return ( 
 		<div className="create">
 			<h2> Add Blog</h2>
@@ -12,16 +14,26 @@ const Create1 = () => {
 				required
 				value = {title}
 				onChange = {(e)=> setTitle(e.target.value)}
-				></input>
+				/>
 				<label>Blog Body</label>
-				<textarea required></textarea>
+				<textarea required
+				value = {body}
+				onChange = {(e)=> setBody(e.target.value)}
+				></textarea>
 				<label> Blog Author</label>
-				<select>
+				<select
+				value = {author}
+				onChange = {(e)=> setAuthor(e.target.value)}
+				>
 					<option value="mario">Mario</option>
 					<option value="yoshi">Yoshi</option>
 				</select> 
 				<button>Add blog</button>
 			</form>
+			{/* to check wehther we can get the input filed value */}
+			<p>{title}</p>
+			<p>{body}</p>
+			<p>{author}</p>
 		</div>
 	 );
 }
